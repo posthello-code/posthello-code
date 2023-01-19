169da2cf6b8646f96fcd9d1be87e0b073df9dd7d
TheStudiousGecko <60212040+posthello-code@users.noreply.github.com>
2023-01-18 21:06:17 -0800

    Update misc_item_button.js

--- misc_item_button.js
+++ misc_item_button.js
@@ -1,3 +1,26 @@
+'use strict';
+
+const e = React.createElement;
+
+class MiscItemButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
 const domContainer = document.querySelector('#misc_item_container');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(e(MiscItemButton));
