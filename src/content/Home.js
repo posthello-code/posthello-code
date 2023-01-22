141d139b9a0b119c0f1022edf70b29bba9ea660f
ajensen <ajensen@ftsinc.com>
2023-01-21 23:44:07 -0800

    update text

--- src/content/Home.js
+++ src/content/Home.js
@@ -5,46 +5,46 @@ function Home() {
   return (
     <div>
       <h1>
-        Here's some thing I'm focusing on right now...
+        Here's some details about me:
       <hr></hr>
       </h1>
       <table>
         <tbody>
           <tr>
-            <th><h2>Software Developer/Test Engineer</h2></th>
+            <th><h2>Software Developer</h2></th>
           </tr>
           <tr>
-            <td>As a developer in test, I have spearheaded initiatives to automate large suites of Acceptance tests, as well as contributed features and bug fixes to various 
+            <td>I have spearheaded initiatives to automate large suites of acceptance tests for front-end/back-end, as well as contributed features and bug fixes to various 
               data collection platforms and user interfaces in the environmental monitoring industries (fire weather, meterology, hydrology, utilities etc). 
-              In addition to this I have learned and contributed to observability, continuous integration/deployment, security other devops tasks.</td>
+              In addition to this I have contributed to observability, continuous integration/deployment, and DevOps infrastructure as time permits.</td>
           </tr>
         </tbody>
       </table>
       <table>
         <tbody>
           <tr>
-            <th><h2>Product Ownership</h2></th>
+            <th><h2>Product Owner</h2></th>
           </tr>
           <tr>
-            <td>As a Product Validation specialist for over 5 years, I have learned a ton about what it takes to develop high-quality software accross the entire stack 
+            <td>As a Product Validation specialist for over 5 years, I have learned a lot about what it takes to develop high-quality software accross the entire stack 
               while maintaining consistency and avoiding scope-creep. I can be relied upon to make informed decisions regarding 
-              product backlogs, feature suggestions, and release management. I find criticism is a useful tool for understanding how to build better products.</td>
+              product backlogs, feature suggestions, and release management. I find continuous feedback is a useful tool for understanding how to build better products.</td>
           </tr>
         </tbody>
       </table>
       <table>
         <tbody>
           <tr>
-            <th><h2>Primary Tools/Technologies</h2></th>
+            <th><h2>Technologies I've been using lately</h2></th>
           </tr>
           <tr>
             <td>
-              Javascript -- Typescript, angular, react, CucumberJS, mochaJS, SerenityJS, WebdriverIO
+              Javascript -- Typescript, Angular, React, CucumberJS, SerenityJS, WebdriverIO
             </td>
           </tr>
           <tr>
             <td>
-              DevOps -- Drone.io/Bitbucket Pipelines, AWS (ec2, lambda, elastic beanstalk, IAM), Kubernetes, Docker
+              DevOps -- Drone.io/Bitbucket Pipelines, AWS (ec2, lambda, elastic beanstalk, IAM), Kubernetes, Docker, Heroku, Render
             </td>
           </tr>
           <tr>
