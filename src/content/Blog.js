e33b3e1cb6e79ba717f267be4992f480ad6a26ac
alex <mail@alexjensen.net>
2023-02-25 15:21:02 -0800

    add a blog page

---
+++ src/content/Blog.js
@@ -0,0 +1,21 @@
+function Amplenote() {
+  return (
+    <div>
+      <div class="amplenote-embed" data-note-token="Tt9PcjS7G1e8ZNXWe9MB2E1X" data-styles="../../Amplenote.css">
+        <iframe frameborder="0" src="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css"></iframe>
+      </div>
+      <script defer src="https://public.amplenote.com/embed.js"></script>
+    </div>)
+}
+
+function Blog() {
+  return (
+    <div>
+      Here's my amplenote
+      <Amplenote>
+      </Amplenote>
+    </div>
+  );
+}
+
+export default Blog;
