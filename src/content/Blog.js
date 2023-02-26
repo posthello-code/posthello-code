d7542734311c052f4d41b5865bea250435b8c3f5
alex <mail@alexjensen.net>
2023-02-25 16:18:56 -0800

    Add blog page

--- src/content/Blog.js
+++ src/content/Blog.js
@@ -2,16 +2,16 @@ function Amplenote() {
   return (
     <div>
       <div class="amplenote-embed" data-note-token="Tt9PcjS7G1e8ZNXWe9MB2E1X" data-styles="../../Amplenote.css">
-        <iframe frameborder="0" src="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css"></iframe>
+        <iframe  width="100%" height={1080} className="amplenote-iFrame" frameborder="0" src="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css"></iframe>
       </div>
-      <script defer src="https://public.amplenote.com/embed.js"></script>
+      <script defer src="https://public.amplenote.com/embed.js">
+      </script>
     </div>)
 }
 
 function Blog() {
   return (
     <div>
-      Here's my amplenote
       <Amplenote>
       </Amplenote>
     </div>
