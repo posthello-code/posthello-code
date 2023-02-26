74cfd0bb991062f04675136f88d9e639f229245d
alex <mail@alexjensen.net>
2023-02-26 15:20:25 -0800

    add grommet

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
+import { Button, Text } from 'grommet';
+
+export class GrommetButtonNav extends Component {
+  render() {
+    const { text, path } = this.props;
+    return (
+      <Link to={path}>
+        <Button label={
+          <Text color='white'>{text}</Text>} color="white"></Button>
+      </Link>
+    )
+  }
+}
 
 export class NavBar extends Component {
-
   render() {
     return (
       <div className="Nav">
-         <div className='navLink'><Link to="/">Home</Link></div>
-         <div className='navLink'><Link to="/blog">Blog</Link></div>
-         <div className='navLink'><Link to="/contact">Contact</Link></div>
-         <hr></hr>
+        <div className='navLink'>
+          <GrommetButtonNav text="Home" path="/"></GrommetButtonNav>
+        </div>
+        <div className='navLink'>
+          <GrommetButtonNav text="Blog" path="/blog"></GrommetButtonNav>
+        </div>
+        <div className='navLink'>
+          <GrommetButtonNav text="Contact" path="/contact"></GrommetButtonNav>
+        </div>
+        <hr></hr>
       </div>
     )
   }
