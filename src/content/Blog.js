634766ed9a30e6b191002679c2eec5dc5d5ec7e8
alex <mail@alexjensen.net>
2023-02-26 22:27:39 -0800

    css update

--- src/content/Blog.js
+++ src/content/Blog.js
@@ -4,7 +4,6 @@ function Amplenote() {
   return (
     <div>
       <h2>My tech diary / and other curious ramblings </h2>
-
       <div className='Box'>
         <Box
           round="medium"
@@ -14,7 +13,7 @@ function Amplenote() {
           width="90%"
           align="center"
           pad="medium"
-          background="light-6"
+          background="dark-2"
           gap="medium">
           <div class="amplenote-embed" data-note-token="Tt9PcjS7G1e8ZNXWe9MB2E1X" data-styles="../../Amplenote.css">
             <iframe title="amplenote" width="100%" height="100%" className="amplenote-iFrame" frameborder="0" src="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css"></iframe>
