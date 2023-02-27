3046a66beabfbdce205b590463062546095cc2c5
alex <mail@alexjensen.net>
2023-02-27 01:25:28 -0800

    css changes

--- src/content/Blog.js
+++ src/content/Blog.js
@@ -15,9 +15,9 @@ function Amplenote() {
           pad="medium"
           background="dark-2"
           gap="medium"> 
-          <div class="amplenote-embed" data-note-token="Tt9PcjS7G1e8ZNXWe9MB2E1X" data-styles="../../Amplenote.css">
+          <div className  ="amplenote-embed" data-note-token="Tt9PcjS7G1e8ZNXWe9MB2E1X" data-styles="../../Amplenote.css">
             <iframe title="amplenote" width="100%" height="100%" 
-              className="amplenote-iFrame" frameborder="0" 
+              className="amplenote-iFrame" frameBorder="0" 
               src="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css">  
             </iframe>
           </div>
