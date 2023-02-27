01d932d6724c68b5a44efcd6cc82ed9dce38c76c
alex <mail@alexjensen.net>
2023-02-26 23:09:59 -0800

    moved description to the bottom

--- src/content/Blog.js
+++ src/content/Blog.js
@@ -4,11 +4,6 @@ function Amplenote() {
   return (
     <div>
       <h2>My tech diary / and other curious ramblings </h2>
-      <div id="ample-desc">
-      <Text textAlign="center">
-        This blog is updated in near real-time using an 
-        embedded <a href="https://www.amplenote.com/help/custom_css_blogging_platform_use_case">Amplenote</a> markdown file.
-      </Text></div>
       <div className='Box'>
         <Box
           round="medium"
@@ -19,7 +14,7 @@ function Amplenote() {
           align="center"
           pad="medium"
           background="dark-2"
-          gap="medium">
+          gap="medium"> 
           <div class="amplenote-embed" data-note-token="Tt9PcjS7G1e8ZNXWe9MB2E1X" data-styles="../../Amplenote.css">
             <iframe title="amplenote" width="100%" height="100%" 
               className="amplenote-iFrame" frameborder="0" 
@@ -28,6 +23,11 @@ function Amplenote() {
           </div>
           <script defer src="https://public.amplenote.com/embed.js">
           </script></Box>
+      </div>
+      <div id="ample-desc">
+      <Text size="small" textAlign="center">
+        This blog is updated in near real-time using an <a href="https://www.amplenote.com/help/custom_css_blogging_platform_use_case">embedded Amplenote</a> markdown <a href="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css">file</a>
+      </Text>
       </div></div>)
 }
 
