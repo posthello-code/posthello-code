99422d62ec12ac44ee92663b55a0ebd652115c78
alex <mail@alexjensen.net>
2023-02-26 22:55:11 -0800

    add description to blog page

--- src/content/Blog.js
+++ src/content/Blog.js
@@ -1,9 +1,14 @@
-import { Box } from 'grommet';
+import { Box, Text } from 'grommet';
 
 function Amplenote() {
   return (
     <div>
       <h2>My tech diary / and other curious ramblings </h2>
+      <div id="ample-desc">
+      <Text textAlign="center">
+        This blog is updated in near real-time using an 
+        embedded <a href="https://www.amplenote.com/help/custom_css_blogging_platform_use_case">Amplenote</a> markdown file.
+      </Text></div>
       <div className='Box'>
         <Box
           round="medium"
@@ -16,7 +21,10 @@ function Amplenote() {
           background="dark-2"
           gap="medium">
           <div class="amplenote-embed" data-note-token="Tt9PcjS7G1e8ZNXWe9MB2E1X" data-styles="../../Amplenote.css">
-            <iframe title="amplenote" width="100%" height="100%" className="amplenote-iFrame" frameborder="0" src="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css"></iframe>
+            <iframe title="amplenote" width="100%" height="100%" 
+              className="amplenote-iFrame" frameborder="0" 
+              src="https://public.amplenote.com/embed/Tt9PcjS7G1e8ZNXWe9MB2E1X?hostname=alexjensen.net&styles=%2FAmplenote.css">  
+            </iframe>
           </div>
           <script defer src="https://public.amplenote.com/embed.js">
           </script></Box>
